ce079f3eacf94b94bcf66b2436a06d8e13f4f091
Jose Garcia <jose.balius@gmail.com>
2014-12-04 21:48:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
 var transform       = require('jstransform').transform;
 var reactTransform  = require('react-tools').transform;
 var visitors        = require('react-tools/vendor/fbtransform/visitors');
-var typeSyntax      = require('jstransform/visitors/type-syntax');
 var through         = require('through');
 
 var isJSXExtensionRe = /^.+\.jsx$/;
 
 function process(file, isJSXFile, transformer) {
-  transformer = transformer || reactTransform;
+  transformer = reactTransform;
 
   var data = '';
   function write(chunk) {
@@ -20,7 +19,7 @@ function process(file, isJSXFile, transformer) {
     // jshint -W040
     if (isJSXFile) {
       try {
-        var transformed = transformer(data);
+        var transformed = transformer(data, {stripTypes: true, harmony: true, es5: true});
         this.queue(transformed);
       } catch (error) {
         error.name = 'ReactifyError';
@@ -42,8 +41,6 @@ function process(file, isJSXFile, transformer) {
 function getExtensionsMatcher(extensions) {
   return new RegExp('\\.(' + extensions.join('|') + ')$');
 }
-
-var VISITORS_OPTION_WARNED = false;
 
 module.exports = function(file, options) {
   options = options || {};
@@ -69,28 +66,18 @@ module.exports = function(file, options) {
       visitors.transformVisitors.react);
 
   if (options.visitors) {
-    if (!VISITORS_OPTION_WARNED) {
-      VISITORS_OPTION_WARNED = true;
-      console.warn(
-        'reactify: option "visitors" is deprecated ' +
-        'and will be removed in future releases'
-      );
-    }
     [].concat(options.visitors).forEach(function(id) {
       transformVisitors = require(id).visitorList.concat(transformVisitors);
     });
   }
 
   var transformOptions = {
-    es5: options.target === 'es5'
+    es5: true,
+    stripTypes: true,
+    harmony: true
   };
 
   function transformer(source) {
-    // Stripping types needs to happen before the other transforms
-    if (options['strip-types'] || options.stripTypes) {
-      source = transform(typeSyntax.visitorList, source, transformOptions).code;
-    }
-
     return transform(transformVisitors, source, transformOptions).code;
   }
 
